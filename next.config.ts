fec0439698bb0da4e9ff960e50e701800037e2f6
Vishwa Lingam <vishwalingam24816@gmail.com>
2025-09-13 16:53:22 +0000

    Try fixing this error: `Runtime Error: Error: Element type is invalid: e

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
